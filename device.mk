49892a161704809616a2f60e65f7a1008a2e504b
xyyx <xyyx@mail.ru>
2020-10-20 10:24:37 +0300

    phoenix: Add charger_res_images to PRODUCT_PACKAGES - Use default charger mode images

--- device.mk
+++ device.mk
@@ -57,6 +57,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     GCamGOPrebuilt
 
+# Charger
+PRODUCT_PACKAGES += \
+    charger_res_images
+
 # Device Settings
 PRODUCT_PACKAGES += \
     XiaomiParts
