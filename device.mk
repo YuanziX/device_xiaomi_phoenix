eb327804d72a6cd7d751543646e0cb8f1ae88852
KakatkarAkshay <akshayakakatkaraa@gmail.com>
2021-01-19 03:54:38 +0100

    phoenix: Import gpio-keys from stock
    * Fixes some wired earphone buttons
    
    Signed-off-by: chiru2000 <chiranthangadi123@gmail.com>
    Signed-off-by: AtomicXZ <achubadyal4@gmail.com>

--- device.mk
+++ device.mk
@@ -99,6 +99,7 @@ PRODUCT_PACKAGES += \
 
 # Keylayout
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/keylayout/gpio-keys.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/gpio-keys.kl \
     $(LOCAL_PATH)/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/uinput-fpc.kl \
     $(LOCAL_PATH)/keylayout/uinput-goodix.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/uinput-goodix.kl
 
