nitrogen-project/android_device_xiaomi_surya

power: Update from CAF

device.mk
@@ -130,7 +130,7 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.2-service.surya
+    android.hardware.power-service.surya
 
 # Product init
 PRODUCT_COPY_FILES += \

power/Android.mk
@@ -1,23 +1,45 @@
 LOCAL_PATH := $(call my-dir)
 
-
-# HAL module implemenation stored in
-# hw/<POWERS_HARDWARE_MODULE_ID>.<ro.hardware>.so
 include $(CLEAR_VARS)
-LOCAL_MODULE := android.hardware.power@1.2-service.surya
+LOCAL_MODULE := android.hardware.power-service.surya
 LOCAL_MODULE_TAGS := optional
 
 LOCAL_MODULE_PATH := $(TARGET_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/bin
 LOCAL_MODULE_RELATIVE_PATH := hw
-LOCAL_MODULE_STEM := android.hardware.power@1.2-service
+LOCAL_MODULE_STEM := android.hardware.power-service
 
-LOCAL_REQUIRED_MODULES := android.hardware.power@1.2-service.rc
-LOCAL_SHARED_LIBRARIES := liblog libcutils libdl libxml2 libbase libhidlbase libutils android.hardware.power@1.2
-LOCAL_HEADER_LIBRARIES += libutils_headers
-LOCAL_HEADER_LIBRARIES += libhardware_headers
-LOCAL_SRC_FILES := power-common.c metadata-parser.c utils.c list.c hint-data.c powerhintparser.c service.cpp Power.cpp
-LOCAL_C_INCLUDES := external/libxml2/include \
-                    external/icu/icu4c/source/common
+LOCAL_REQUIRED_MODULES := android.hardware.power-service.rc
+
+LOCAL_SHARED_LIBRARIES := \
+    liblog \
+    libcutils \
+    libdl \
+    libxml2 \
+    libbase \
+    libbinder_ndk \
+    libutils \
+    android.hardware.power-ndk_platform
+
+LOCAL_HEADER_LIBRARIES += \
+    libutils_headers \
+    libhardware_headers
+
+LOCAL_SRC_FILES := \
+    power-common.c \
+    metadata-parser.c \
+    utils.c \
+    list.c \
+    hint-data.c \
+    powerhintparser.c \
+    main.cpp \
+    Power.cpp \
+    power-msmnile.c
+
+LOCAL_C_INCLUDES := \
+   external/libxml2/include \
+   external/icu/icu4c/source/common
+
+LOCAL_CFLAGS += -Wno-unused-parameter -Wno-unused-variable
 
 ifneq ($(TARGET_TAP_TO_WAKE_NODE),)
     LOCAL_CFLAGS += -DTAP_TO_WAKE_NODE=\"$(TARGET_TAP_TO_WAKE_NODE)\"
@@ -29,10 +51,10 @@ endif
 include $(BUILD_EXECUTABLE)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := android.hardware.power@1.2-service.rc
+LOCAL_MODULE := android.hardware.power-service.rc
+LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH := $(TARGET_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/init
-LOCAL_MODULE_STEM :=  android.hardware.power@1.2-service.rc
-LOCAL_SRC_FILES := android.hardware.power@1.2-service.rc
+LOCAL_MODULE_STEM := android.hardware.power-service.rc
+LOCAL_SRC_FILES := android.hardware.power-service.rc
 include $(BUILD_PREBUILT)
-

YuanziX/device_xiaomi_phoenix

Signed-off-by: chiru2000 <chiranthangadi123@gmail.com>
Signed-off-by: AtomicXZ <achubadyal4@gmail.com>

device.mk
@@ -139,7 +139,7 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.2-service.phoenix
+    android.hardware.power-service.phoenix
 
 # RCS
 PRODUCT_PACKAGES += \

power/Android.mk
@@ -1,23 +1,46 @@
 LOCAL_PATH := $(call my-dir)
 
-
-# HAL module implemenation stored in
-# hw/<POWERS_HARDWARE_MODULE_ID>.<ro.hardware>.so
 include $(CLEAR_VARS)
-LOCAL_MODULE := android.hardware.power@1.2-service.phoenix
+LOCAL_MODULE := android.hardware.power-service.phoenix
 LOCAL_MODULE_TAGS := optional
 
 LOCAL_MODULE_PATH := $(TARGET_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/bin
 LOCAL_MODULE_RELATIVE_PATH := hw
-LOCAL_MODULE_STEM := android.hardware.power@1.2-service
 
-LOCAL_REQUIRED_MODULES := android.hardware.power@1.2-service.rc
-LOCAL_SHARED_LIBRARIES := liblog libcutils libdl libxml2 libbase libhidlbase libhidltransport libutils android.hardware.power@1.2
-LOCAL_HEADER_LIBRARIES += libutils_headers
-LOCAL_HEADER_LIBRARIES += libhardware_headers
-LOCAL_SRC_FILES := power-common.c metadata-parser.c utils.c list.c hint-data.c powerhintparser.c service.cpp Power.cpp
-LOCAL_C_INCLUDES := external/libxml2/include \
-                    external/icu/icu4c/source/common
+LOCAL_MODULE_STEM := android.hardware.power-service
+
+LOCAL_REQUIRED_MODULES := android.hardware.power-service.rc
+
+LOCAL_SHARED_LIBRARIES := \
+    liblog \
+    libcutils \
+    libdl \
+    libxml2 \
+    libbase \
+    libbinder_ndk \
+    libutils \
+    android.hardware.power-ndk_platform
+
+LOCAL_HEADER_LIBRARIES += \
+    libutils_headers \
+    libhardware_headers
+
+LOCAL_SRC_FILES := \
+    power-common.c \
+    metadata-parser.c \
+    utils.c \
+    list.c \
+    hint-data.c \
+    powerhintparser.c \
+    main.cpp \
+    Power.cpp \
+    power-msmnile.c
+
+LOCAL_C_INCLUDES := \
+   external/libxml2/include \
+   external/icu/icu4c/source/common
+
+LOCAL_CFLAGS += -Wno-unused-parameter -Wno-unused-variable
 
 ifneq ($(TARGET_TAP_TO_WAKE_NODE),)
     LOCAL_CFLAGS += -DTAP_TO_WAKE_NODE=\"$(TARGET_TAP_TO_WAKE_NODE)\"
@@ -29,10 +52,10 @@ endif
 include $(BUILD_EXECUTABLE)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := android.hardware.power@1.2-service.rc
+LOCAL_MODULE := android.hardware.power-service.rc
+LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH := $(TARGET_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/init
-LOCAL_MODULE_STEM :=  android.hardware.power@1.2-service.rc
-LOCAL_SRC_FILES := android.hardware.power@1.2-service.rc
+LOCAL_MODULE_STEM := android.hardware.power-service.rc
+LOCAL_SRC_FILES := android.hardware.power-service.rc
 include $(BUILD_PREBUILT)
-