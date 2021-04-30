14b0aff30fe63bb50d457d10cb406bc1bc4007a2
AtomicXZ <achubadyal4@gmail.com>
2021-04-30 12:55:58 +0000

    phoenix: Nezuko bringup
    Signed-off-by: AtomicXZ <achubadyal4@gmail.com>

--- nezuko_phoenix.mk
+++ nezuko_phoenix.mk
@@ -6,20 +6,22 @@
 
 $(call inherit-product, device/xiaomi/phoenix/device.mk)
 
-# Inherit some common ArrowOS stuff.
-$(call inherit-product, vendor/arrow/config/common.mk)
+# Inherit some common Nezuko OS stuff.
+$(call inherit-product, vendor/nezuko/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := arrow_phoenix
+PRODUCT_NAME := nezuko_phoenix
 PRODUCT_DEVICE := phoenix
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := POCO X2
 PRODUCT_MANUFACTURER := Xiaomi
 
-# ArrowOS Properties
-TARGET_GAPPS_ARCH := arm64
+# Nezuko OS Properties
 TARGET_BOOT_ANIMATION_RES := 1080
-DEVICE_MAINTAINER := Sreekanth
+NEZUKO_MAINTAINER := AtomicX
+NEZUKO_BUILD_TYPE := OFFICIAL
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.nezuko.cpu=SD730G
 
 BUILD_FINGERPRINT := google/redfin/redfin:11/RQ2A.210405.005/7181113:user/release-keys
 BUILD_DESCRIPTION := redfin-user 11 RQ2A.210405.005 7181113 release-keys
